944d81ad26dc996d50075d232c4f326741866b78
Eder Souza <ederaldo.souza93@gmail.com>
2026-05-27 23:15:34 -0300

    feat: migrate Bible API to NestJS backend

--- types/index.ts
+++ types/index.ts
@@ -115,3 +115,20 @@ export interface BackendAuthResponse {
   accessToken: string;
   user: User;
 }
+
+export interface BibleVerse {
+  book_id: string;
+  book_name: string;
+  chapter: number;
+  verse: number;
+  text: string;
+}
+
+export interface BibleApiResponse {
+  reference: string;
+  verses: BibleVerse[];
+  text: string;
+  translation_id: string;
+  translation_name: string;
+  translation_note: string;
+}
